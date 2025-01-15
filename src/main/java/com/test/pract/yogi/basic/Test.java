e5d7e1b58a02416bf5ca3c744d7ae3852a676a44
Yogeswari <yogeswari123@gmail.com>
2025-01-15 19:42:40 +0530

    First java program uploaded

--- src/main/java/com/test/pract/yogi/basic/Test.java
+++ src/main/java/com/test/pract/yogi/basic/Test.java
@@ -1,8 +1,10 @@
 package com.test.pract.yogi.basic;
 
-public class Test {
-    public static void main(String[] args)
+public class Test
     {
-        System.out.println("Welcome to the codings journey!");
+        public static void main(String[] args)
+        {
+            int String=10;
+            System.out.println(String);
+        }
     }
-}
