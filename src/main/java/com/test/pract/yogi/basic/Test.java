df9367866b85c04e2bd28c7beffc9379ec0f69e5
Yogeswari <yogeswari123@gmail.com>
2025-01-15 20:43:50 +0530

    First java program uploaded

--- src/main/java/com/test/pract/yogi/basic/Test.java
+++ src/main/java/com/test/pract/yogi/basic/Test.java
@@ -1,10 +1,12 @@
 package com.test.pract.yogi.basic;
 
-public class Test
-    {
-        public static void main(String[] args)
-        {
-            int Runnable= 20+30-20;
-            System.out.println(Runnable);
-        }
+public class Test {
+    public static void main(String[] args) {
+        int[] a = new int[3];
+        int[] b = new int[6];
+        int[] c = new int[7];
+        System.out.println(a.length);
+        System.out.println(b.length);
+        System.out.println(c.length);
     }
+}
