218d5337976691cefe58f37bfd57d1158f0380c9
Yogeswari <yogeswari123@gmail.com>
2025-01-17 21:19:52 +0530

    First java program uploaded

---
+++ src/main/java/com/test/pract/yogi/basic/arrays/Multidimensional.java
@@ -0,0 +1,17 @@
+package com.test.pract.yogi.basic.arrays;
+
+public class Multidimensional {
+    public static void main(String[] args)
+    {
+       int[] a = new int[5];
+       System.out.println(a.length);
+
+    }
+
+
+
+
+
+
+
+}
